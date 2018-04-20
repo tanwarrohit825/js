2f3f1c7ecf3ce6e545780acc0a830bd470a1e082
Rohit Tanwar <tanwarrohit825@gmail.com>
2018-04-20 13:38:54 +0530

    Kings Problem
    always declear the variable in
    
    let --local variable
    
    var -- global variable
    
    note -----
          1. not define the variable without using (let and var )
         2.due to this leack the data

--- 02basic/scope.js
+++ 02basic/scope.js
@@ -1,14 +1,38 @@
-let iAmGloble = 'someValue'
+// let iAmGloble = 'someValue'
+
+// if(true){
+//     let iAmLocal = 'some more Value'
+//     iAmGloble= 'superman'
+//     console.log(iAmGloble);
+//     console.log(iAmLocal);
+    
+    
+// }
+// console.log(iAmGloble);
+
+//Kings problem
+
+let king = 'John'
 
 if(true){
-    let iAmLocal = 'some more Value'
-    iAmGloble= 'superman'
-    console.log(iAmGloble);
-    console.log(iAmLocal);
-    
-    
+    // let king = 'San'
+
+    if(true){
+        // let king = 'Ram'
+        console.log(king);
+        
+
+    }
+    if (true) {
+        console.log("I am second part"+king);
+        
+        
+    }
+
 }
-console.log(iAmGloble);
+
+
+
 
 
 
