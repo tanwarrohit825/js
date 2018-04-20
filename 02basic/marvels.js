2f64e4862f3d31cb04018cb9c6eb1a13b16ba622
Rohit Tanwar <tanwarrohit825@gmail.com>
2018-04-20 14:15:51 +0530

    Arrays
    arrays and using of this ( ` ` )

---
+++ 02basic/marvels.js
@@ -0,0 +1,20 @@
+//datatype -- Array
+
+const superHeroes = ['Iron Man','Spiderman','capt. America','Thor']
+
+// console.log(superHeroes);
+
+
+// console.log(superHeroes[0]);
+//console.log(superHeroes[superHeroes.length-1]);
+
+a =superHeroes[superHeroes.length - 1]
+
+console.log(`we have ${superHeroes[superHeroes.length - 1]} is  ${superHeroes.length} hero`);
+
+console.log(`we have ${a} is  ${superHeroes.length} hero`);
+
+
+
+
+
