90b8b75a17e2aade15e6c186748aaafb143f4565
Rohit Tanwar <tanwarrohit825@gmail.com>
2018-04-21 11:23:44 +0530

    learing the funtion
    how to use funtion in js

---
+++ 01Intermediate/funtion.js
@@ -0,0 +1,25 @@
+// let sayHello = function(name){
+//     console.log(`greetinng message ${name}`);
+
+//     console.log(`Hey ${name}`);
+
+// }
+// // sayHello("Rohit")
+
+// let fullNmeMaker = function(firstNamr,lastName){
+//     console.log("Welocome");
+//     console.log(`happy to see you ${firstNamr} ${lastName}`);
+    
+    
+// }
+// fullNmeMaker("rohit"," Tanwar")
+
+let myAdder = function(num1,mun2){
+    let added = num1+ mun2
+    return added
+
+}
+let resuklt = myAdder(3,5)
+console.log(`result is ${resuklt}`);
+
+
