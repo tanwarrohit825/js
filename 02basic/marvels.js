f2660388cc991e9ba4a9acef417baca407365403
Rohit Tanwar <tanwarrohit825@gmail.com>
2018-04-21 15:20:54 +0530

    push and po in array
    how to add/ sub/replace in the end of array

--- 02basic/marvels.js
+++ 02basic/marvels.js
@@ -1,18 +1,58 @@
-//datatype -- Array
+// //datatype -- Array
 
-const superHeroes = ['Iron Man','Spiderman','capt. America','Thor']
+// const superHeroes = ['Iron Man','Spiderman','capt. America','Thor']
 
-// console.log(superHeroes);
+// // console.log(superHeroes);
 
 
-// console.log(superHeroes[0]);
-//console.log(superHeroes[superHeroes.length-1]);
+// // console.log(superHeroes[0]);
+// //console.log(superHeroes[superHeroes.length-1]);
 
-a =superHeroes[superHeroes.length - 1]
+// a =superHeroes[superHeroes.length - 1]
 
-console.log(`we have ${superHeroes[superHeroes.length - 1]} is  ${superHeroes.length} hero`);
+// console.log(`we have ${superHeroes[superHeroes.length - 1]} is  ${superHeroes.length} hero`);
 
-console.log(`we have ${a} is  ${superHeroes.length} hero`);
+// console.log(`we have ${a} is  ${superHeroes.length} hero`);
+
+
+
+
+
+
+const number = ['one','two', 'three','four','five','six']
+
+// number [1] = 'something'
+
+// console.log(number);
+
+//start 
+// console.log(number.shift());   // shift one plase of element in array from first place
+// console.log(number);
+
+
+// number.unshift('somthiing ')     // add the element in array in first place 
+// console.log(number);
+
+//end 
+// console.log("the element is to deleated is ---  " +number.pop());
+// console.log(number);
+
+
+// number.push('seven')
+// console.log(number);
+
+//middle 
+
+// number.splice(2,2,'somthing','second')
+// console.log(number);
+
+
+
+
+
+
+
+
 
 
 
